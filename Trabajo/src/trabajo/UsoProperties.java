b7484e75ea1cb9ea6059b9d400b4a52de033bb4b
jcarlosHueso <jchuesos01@educarex.es>
2023-03-07 09:57:40 +0100

    trabajo final

--- Trabajo/src/trabajo/UsoProperties.java
+++ Trabajo/src/trabajo/UsoProperties.java
@@ -3,6 +3,7 @@ package trabajo;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -19,13 +20,11 @@ public void guardarUsuario(User u) {
 				 file.createNewFile();
 				 properties.load(new FileInputStream(file));
 			 }
-		 
-	      
-	       
-	      
 	      properties.setProperty("email", u.getEmail());
 	      properties.setProperty("user", u.getName());
 	      properties.setProperty("pwd", u.getPassword());
+	      
+	      properties.store(new FileOutputStream(file), null);
 	    } catch (FileNotFoundException e) {
 	      // TODO Auto-generated catch block
 	      e.printStackTrace();
